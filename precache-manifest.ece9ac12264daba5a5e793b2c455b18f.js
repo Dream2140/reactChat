416efd1f95a8b85e42a67df7431019d798e58867
hleb.antonenko <glebantonenko2140@gmail.com>
2022-07-07 20:41:42 +0300

    Updates

---
+++ precache-manifest.ece9ac12264daba5a5e793b2c455b18f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2030301cca4f2f9106b29fe47381705",
+    "url": "/reactChat/index.html"
+  },
+  {
+    "revision": "6caa1ec153332baab6bc",
+    "url": "/reactChat/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "adb0a54538eafced32d2",
+    "url": "/reactChat/static/js/2.111e7743.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/reactChat/static/js/2.111e7743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6caa1ec153332baab6bc",
+    "url": "/reactChat/static/js/main.936b8aab.chunk.js"
+  },
+  {
+    "revision": "ac46e041e672f3afbded",
+    "url": "/reactChat/static/js/runtime-main.3649e7d4.js"
+  }
+]);
